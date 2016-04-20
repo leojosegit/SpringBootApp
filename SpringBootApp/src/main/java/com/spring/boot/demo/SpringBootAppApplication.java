b7c9f01a542d70b44c1190497ead31d62124a74c
narendraraghu <narendraraghu.89@gmail.com>
2016-04-20 22:41:33 +0530

    test file

--- SpringBootApp/src/main/java/com/spring/boot/demo/SpringBootAppApplication.java
+++ SpringBootApp/src/main/java/com/spring/boot/demo/SpringBootAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
-
+/*application start here*/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
